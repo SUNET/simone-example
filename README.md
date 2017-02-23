e6bc2c785d15931c7cdfbaf1ea2e96044fae4560
Mattias Andersson <mattias.andersson@umu.se>
2017-02-23 08:10:24 +0100

    renamed projects

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # simone-example
+
+## Description
+
+## Build
+Builds a Wildfly Docker container.
+
+```bash
+mvn package docker:build 
+```
+## Run
+
+Run the example on port 8080
+
+```bash
+docker run -it --rm -p 8080:8080 simone-example
+```
+
+### Base URI
+If you change the port or SimOne is installed behind a firewall you must change the base URI used in the published feed. You can do that by adding the following to the run command.
+```bash
+-e "SIMONE_BASE_URI=http://my.uri.se"
+```
+## Try it out	
+
+### Add Order 
+
+### Read Feed
+
+### Batch load Orders
+
+## Tips
+
+## Debug
+
+remote debug
--- src/main/java/se/uhr/simone/restbucks/boundary/ExampleApplication.java
+++ src/main/java/se/uhr/simone/restbucks/boundary/ExampleApplication.java
@@ -6,14 +6,14 @@ import javax.ws.rs.core.Application;
 import io.swagger.jaxrs.config.BeanConfig;
 
 @ApplicationPath("/api")
-public class RestbucksApplication extends Application {
+public class ExampleApplication extends Application {
 
-	public RestbucksApplication() {
+	public ExampleApplication() {
 		BeanConfig beanConfig = new BeanConfig();
 		beanConfig.setSchemes(new String[] { "http" });
 		beanConfig.setHost("localhost:8080");
 		beanConfig.setBasePath("/sim/api");
-		beanConfig.setResourcePackage("se.uhr.simone,se.uhr.nya.integration.sim.server");
+		beanConfig.setResourcePackage("se.uhr.simone,se.uhr.simone.restbucks");
 		beanConfig.setScan(true);
 	}
 }
