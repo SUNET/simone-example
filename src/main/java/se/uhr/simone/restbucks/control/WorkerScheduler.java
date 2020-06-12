5c18253a220835fbe565da53ab12b89773b7e02e
Mattias Andersson <mattias.andersson@umu.se>
2020-06-12 15:28:58 +0200

    quarkus 1.5.1 make sure SimoneTimerEvent executes in a single thread

--- src/main/java/se/uhr/simone/restbucks/control/WorkerScheduler.java
+++ src/main/java/se/uhr/simone/restbucks/control/WorkerScheduler.java
@@ -1,8 +1,13 @@
 package se.uhr.simone.restbucks.control;
 
+import java.util.concurrent.Semaphore;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.quarkus.scheduler.Scheduled;
 import se.uhr.simone.api.SimoneTimerEvent;
@@ -10,11 +15,20 @@ import se.uhr.simone.api.SimoneTimerEvent;
 @ApplicationScoped
 public class WorkerScheduler {
 
+	private final static Logger LOG = LoggerFactory.getLogger(WorkerScheduler.class);
+
 	@Inject
 	private Event<SimoneTimerEvent> simoneTimerEvent;
 
+	private final Semaphore semaphore = new Semaphore(1);
+
 	@Scheduled(every = "{simone.worker.schedule.expr}")
 	void onStart() {
-		simoneTimerEvent.fire(new SimoneTimerEvent());
+		if (semaphore.tryAcquire()) {
+			simoneTimerEvent.fire(new SimoneTimerEvent());
+			semaphore.release();
+		} else {
+			LOG.trace("could not acquire permit");
+		}
 	}
 }
